f701af332e47ee5bfa7272b760a9d3558ffe5916
Gabriel Rozeno <gabrozenodev@gmail.com>
2024-11-14 00:48:50 -0300

    set up database queries to get single task and all tasks

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 class Task
 {
     public function __construct(
@@ -15,62 +16,20 @@ class Task
     ) {
     }
 }
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
+
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function ()  {
     return view('index', [
-        'tasks'=> $tasks,
+        'tasks'=> \App\Models\Task::latest()->get(),
     ]);
 })->name('tasks.index');
 
-Route::get('/task/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
-    if (!$task) {
-        return abort(404);
-    } else {
+Route::get('/task/{id}', function ($id)  {
         return view('single-task', [
-            'task' => $task
+            'task' =>     \App\Models\Task::findOrFail($id)
         ]);
-    }
 })->name('task.show');
